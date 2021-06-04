15ede7c3bf72a62872cc89b0b7d8ad28d1d14512
Bartłomiej Binczyk <bartekbn@gmail.com>
2021-06-04 14:38:57 +0200

    init

---
+++ src/main/java/com/example/cycleshop/CycleShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.cycleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycleShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CycleShopApplication.class, args);
+    }
+
+}
