d16b5efeebffabd984f170b497fd9a6f7d591c69
Bartłomiej Binczyk <bartekbn@gmail.com>
2021-06-04 21:25:25 +0200

    add JPA to BIkeRepository

--- src/main/java/com/example/cycleshop/api/BikeRepository.java
+++ src/main/java/com/example/cycleshop/api/BikeRepository.java
@@ -1,4 +1,9 @@
 package com.example.cycleshop.api;
 
-public interface BikeRepository {
+import com.example.cycleshop.api.model.Bike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BikeRepository extends JpaRepository<Bike, Long> {
 }
